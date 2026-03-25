d254b2f70603d90cee1c1f6455a3791b39bf3f7b
Hanna <st9172611@stud.duikt.edu.ua>
2026-03-25 09:08:11 +0200

    add pr2 task2 and update task1

---
+++ pr2_sem2/pr2_2/SoundSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr2._2._2
+{
+    class SoundSystem
+    {
+        public void PlaySound(int damage, int hp)
+        {
+            Console.WriteLine("[Sound] Damage sound ");
+
+            if (hp <= 20)
+                Console.WriteLine("[Sound] CRITICAL HP sound");
+        }
+    }
+}
